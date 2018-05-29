0cb3ca5d150f63bca403b7721e3aa1e152e9384d
threadpool2 <39743690+threadpool2@users.noreply.github.com>
2018-05-30 00:46:08 +0530

    Add files via upload

---
+++ reverse.py
@@ -0,0 +1,11 @@
+
+def reverse(n):    
+    rev=0
+    a=0
+    while n>0:
+        a=n%10
+        rev=rev*10+a
+        n=n//10
+    print("Reverse of a entered number is",rev)
+n=int(input("Enter any number:"))
+reverse(n)
